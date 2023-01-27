4859226eaeba100f09018f2c3e3d931843e940df
Flavio Andrade <foa12381@gmail.com>
2023-01-27 08:48:18 -0300

    feat/test/carrinho

--- test/carrinho.test.js
+++ test/carrinho.test.js
@@ -10,17 +10,35 @@ describe("Carrinho", () => {
   it ("Deve conter itens adicionado no carrinho", ()=> {
     const item1 = new Item('Uva', 4.3, 2)
     const item2 = new Item('Açaí', 3, 5)
+    const item3 = new Item('manga', 2, 10)
+
     const carrinho = new Carrinho()
 
     carrinho.adiciona(item1)
     carrinho.adiciona(item2)
+    carrinho.adiciona(item3)
 
-    expect(typeof carrinho).toBe("object")
+
     expect(carrinho instanceof Carrinho).toBe(true)
+    expect(typeof carrinho).toBe("object")
     expect(carrinho.itens[0]).toBe(item1)
     expect(carrinho.itens[1]).toBe(item2)
+    expect(carrinho.itens).toContain(item3)
+  })
 
+  it('Deve ter as Propiedades subtotal', ()=> {
+    const carrinho = new Carrinho()
 
+    expect(carrinho).toHaveProperty('subtotal')
+  })
 
+  it('Deve lancar error ao finalizar carrinho vazio', ()=> {
+    const carrinho = new Carrinho()
+
+    function finalizarCar() {
+      const result = carrinho.finalizaCompra()
+    }
+
+    expect(finalizarCar).toThrowError('Carrinho de compras vazio')
   })
 })
